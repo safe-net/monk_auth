a2d75956974541dd72e2e7bfb55691b9bd383585
Russ Egan <russegan@Russs-MacBook-Pro.local>
2014-06-23 15:09:20 -0400

    added tokens and stuff

--- snaplogin/app/controllers/root_controller.rb
+++ snaplogin/app/controllers/root_controller.rb
@@ -1,5 +1,21 @@
 class RootController < ApplicationController
   def index
-    @user_session = UserSession.new
+    token = cookies.signed[:snap_login]
+    if token.present?
+      @snap_login = SnapLogin.find_by_token(token)
+    end
+    if @snap_login.nil?
+      @snap_login = SnapLogin.new
+      @snap_login.token = UUIDTools::UUID.random_create.to_s
+      @snap_login.save
+      cookies.signed[:snap_login] = @snap_login.token
+    end
+    if @snap_login && @snap_login.email.present?
+      @user_session = UserSession.create(User.find_by_email(@snap_login.email), true)
+      @snap_login.destroy
+      cookies.delete :snap_login
+    else
+      @user_session = UserSession.new
+    end
   end
 end
