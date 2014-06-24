5f0dcae010a6ca26e4eb6ac1aed7b5e39363a7ca
Russ Egan <russ@safemonk.com>
2014-06-24 17:09:15 -0400

    Use cocoa pod for bar scanner library

--- clients/ios/MonkAuthReader/EmbedReader/AuthenticationManager.m
+++ clients/ios/MonkAuthReader/EmbedReader/AuthenticationManager.m
@@ -23,7 +23,7 @@
 }
 
 -(RACSignal *)authenticate:(NSString *)url {
-    return [self enqueueRequestWithMethod: @"PUT" path:url parameters:@{@"email":"james@safemonk.com"}];
+    return [self enqueueRequestWithMethod: @"PUT" path:url parameters:@{@"email":@"russ@safemonk.com"}];
 }
 
 - (RACSignal *)enqueueRequestWithMethod:(NSString *)method path:(NSString *)path parameters:(NSDictionary *)parameters {
